c70d596a7ebd45eef3bee12938a0615cfd1a3467
umar <ciit.umar.047@gmail.com>
2023-12-14 21:43:58 +0500

    Changes related to design

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router'; // Import 'Routes'
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
@@ -11,12 +11,16 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from './services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
 
 const appRoutes: Routes = [ // Define as an array of Routes
   { path: '', component: PropertyListComponent },
   { path: 'add-property', component: AddPropertyComponent },
   { path: 'rent-property', component: PropertyListComponent },
   { path: 'property-detail/:id', component: PropertyDetailComponent },
+  { path: 'user/login', component: UserLoginComponent },
+  { path: 'user/register', component: UserRegisterComponent },
   { path: '**', component: PropertyListComponent }
 ];
 
@@ -27,12 +31,15 @@ const appRoutes: Routes = [ // Define as an array of Routes
     PropertyListComponent,
     NavBarComponent,
     AddPropertyComponent,
-    PropertyDetailComponent
+    PropertyDetailComponent,
+    UserRegisterComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
---
+++ Frontend/src/app/user/user-register/user-register.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, AbstractControl, ValidationErrors, Validators, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-register',
+  templateUrl: './user-register.component.html',
+  styleUrls: ['./user-register.component.css']
+})
+export class UserRegisterComponent implements OnInit {
+
+  registrationForm: FormGroup;
+  user: any={};
+
+  constructor( private fb:FormBuilder) {
+    this.registrationForm = new FormGroup({
+      userName: new FormControl()
+    });
+  }
+
+  ngOnInit() {
+    // this.registrationForm = new FormGroup({
+    //   userName: new FormControl('Mark', Validators.required),
+    //   email: new FormControl(null, [Validators.required, Validators.email]),
+    //   password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+    //   confirmPassword: new FormControl(null, [Validators.required]),
+    //   mobile: new FormControl(null, [Validators.required, Validators.maxLength(10)]),
+    // }, { validators: this.passwordMatchingValidator });
+    this.createRegisterationForm();
+  }
+
+createRegisterationForm(){
+  this.registrationForm=this.fb.group({
+   userName:[null,Validators.required],
+   email:[null,[Validators.required,Validators.email]],
+   password:[null,[Validators.required,Validators.maxLength(8)]],
+   confirmPassword:[null,Validators.required],
+   mobile:[null,[Validators.required,Validators.maxLength(10)]]
+  },{validators:this.passwordMatchingValidator})
+}
+
+  passwordMatchingValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { notmatched: true };
+  }
+
+get userName(){
+  return this.registrationForm.get('userName') as FormControl;
+}
+
+get email(){
+  return this.registrationForm.get('email') as FormControl;
+}
+get password(){
+  return this.registrationForm.get('password') as FormControl;
+}
+get confirmPassword(){
+  return this.registrationForm.get('confirmPassword') as FormControl;
+}
+get mobile(){
+  return this.registrationForm.get('mobile') as FormControl;
+}
+onSubmit() {
+  console.log(this.registrationForm.value);
+  this.user = Object.assign({}, this.user, this.registrationForm.value);
+  this.addUser(this.user);
+}
+
+addUser(user: any) {
+  let users = [];
+  // Check if 'Users' exists in local storage and if it's a string.
+  const storedUsers = localStorage.getItem('Users');
+  if (typeof storedUsers === 'string') {
+    users = JSON.parse(storedUsers);
+    users = [user, ...users];
+  } else {
+    users = [user];
+  }
+
+  localStorage.setItem('Users', JSON.stringify(users));
+}
+
+
+}
